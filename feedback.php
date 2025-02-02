714c03853abfe3e3f736a7858e0d377259a454da
Dev-Daniel <139226309+dev-dantez@users.noreply.github.com>
2025-02-02 12:03:34 +0300

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -2,8 +2,8 @@
 if(!isset($_SESSION)){ 
   session_start(); 
 }
-define('TITLE', 'Courses');
-define('PAGE', 'courses');
+define('TITLE', 'Feedback');
+define('PAGE', 'feedback');
 include('./adminInclude/header.php'); 
 include('../dbConnection.php');
 
@@ -12,32 +12,30 @@ include('../dbConnection.php');
  } else {
   echo "<script> location.href='../index.php'; </script>";
  }
- ?>
-
+?>
   <div class="col-sm-9 mt-5">
     <!--Table-->
-    <p class=" bg-dark text-white p-2">List of Courses</p>
+    <p class=" bg-dark text-white p-2">List of Feedbacks</p>
     <?php
-      $sql = "SELECT * FROM course";
+      $sql = "SELECT * FROM feedback";
       $result = $conn->query($sql);
       if($result->num_rows > 0){
        echo '<table class="table">
        <thead>
         <tr>
-         <th scope="col">Course ID</th>
-         <th scope="col">Name</th>
-         <th scope="col">Author</th>
+         <th scope="col">Feedback ID</th>
+         <th scope="col">Content</th>
+         <th scope="col">Student ID</th>
          <th scope="col">Action</th>
         </tr>
        </thead>
        <tbody>';
         while($row = $result->fetch_assoc()){
           echo '<tr>';
-          echo '<th scope="row">'.$row["course_id"].'</th>';
-          echo '<td>'. $row["course_name"].'</td>';
-          echo '<td>'.$row["course_author"].'</td>';
-          echo '<td><form action="editcourse.php" method="POST" class="d-inline"> <input type="hidden" name="id" value='. $row["course_id"] .'><button type="submit" class="btn btn-info mr-3" name="view" value="View"><i class="fas fa-pen"></i></button></form>  
-          <form action="" method="POST" class="d-inline"><input type="hidden" name="id" value='. $row["course_id"] .'><button type="submit" class="btn btn-secondary" name="delete" value="Delete"><i class="far fa-trash-alt"></i></button></form></td>
+          echo '<th scope="row">'.$row["f_id"].'</th>';
+          echo '<td>'. $row["f_content"].'</td>';
+          echo '<td>'.$row["stu_id"].'</td>';
+          echo '<td><form action="" method="POST" class="d-inline"><input type="hidden" name="id" value='. $row["f_id"] .'><button type="submit" class="btn btn-secondary" name="delete" value="Delete"><i class="far fa-trash-alt"></i></button></form></td>
          </tr>';
         }
 
@@ -47,7 +45,7 @@ include('../dbConnection.php');
         echo "0 Result";
       }
       if(isset($_REQUEST['delete'])){
-       $sql = "DELETE FROM course WHERE course_id = {$_REQUEST['id']}";
+       $sql = "DELETE FROM feedback WHERE f_id = {$_REQUEST['id']}";
        if($conn->query($sql) === TRUE){
          // echo "Record Deleted Successfully";
          // below code will refresh the page after deleting the record
@@ -59,7 +57,6 @@ include('../dbConnection.php');
      ?>
   </div>
  </div>  <!-- div Row close from header -->
- <div><a class="btn btn-danger box" href="./addCourse.php"><i class="fas fa-plus fa-2x"></i></a></div>
 </div>  <!-- div Conatiner-fluid close from header -->
 <?php
 include('./adminInclude/footer.php'); 
